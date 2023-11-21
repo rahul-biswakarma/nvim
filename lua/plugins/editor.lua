c9078ab6937ed9037e4a6166a78fd9ee78f83d20
Rahul Biswakarma <rahul.id39@gmail.com>
2023-11-21 10:18:38 +0530

    first commit

---
+++ lua/plugins/editor.lua
@@ -0,0 +1,225 @@
+return {
+    {
+        enabled = false,
+        "folke/flash.nvim",
+        ---@type Flash.Config
+        opts = {
+            search = {
+                forward = true,
+                multi_window = false,
+                wrap = false,
+                incremental = true
+            }
+        }
+    }, {
+        "echasnovski/mini.hipatterns",
+        event = "BufReadPre",
+        opts = {
+            highlighters = {
+                hsl_color = {
+                    pattern = "hsl%(%d+,? %d+,? %d+%)",
+                    group = function(_, match)
+                        local utils = require("craftzdog.utils")
+                        local h, s, l = match:match(
+                                            "hsl%((%d+),? (%d+),? (%d+)%)")
+                        h, s, l = tonumber(h), tonumber(s), tonumber(l)
+                        local hex_color = utils.hslToHex(h, s, l)
+                        return MiniHipatterns.compute_hex_color_group(hex_color,
+                                                                      "bg")
+                    end
+                }
+            }
+        }
+    }, {
+        "dinhhuy258/git.nvim",
+        event = "BufReadPre",
+        opts = {
+            keymaps = {
+                -- Open blame window
+                blame = "<Leader>gb",
+                -- Open file/folder in git repository
+                browse = "<Leader>go"
+            }
+        }
+    }, {
+        "telescope.nvim",
+        dependencies = {
+            {"nvim-telescope/telescope-fzf-native.nvim", build = "make"},
+            "nvim-telescope/telescope-file-browser.nvim"
+        },
+        keys = {
+            {
+                "<leader>fP",
+                function()
+                    require("telescope.builtin").find_files({
+                        cwd = require("lazy.core.config").options.root
+                    })
+                end,
+                desc = "Find Plugin File"
+            }, {
+                "<leader>fF",
+                function()
+                    require("craftzdog.picker").prettyFilesPicker({
+                        picker = 'git_files'
+                    })
+                end,
+                desc = "Find Git File"
+            }, {
+                "<leader>ff",
+                function()
+                    require("craftzdog.picker").prettyFilesPicker({
+                        picker = 'find_files'
+                    })
+                end,
+                desc = "Find cwd File"
+            }, {
+                "<leader>/",
+                function()
+                    require("craftzdog.picker").prettyGrepPicker({
+                        picker = 'live_grep'
+                    })
+                end,
+                desc = "Live Grep"
+            }, {
+                "<leader>|",
+                function()
+                    require("craftzdog.picker").prettyGrepPicker({
+                        picker = 'grep_string'
+                    })
+                end,
+                desc = "Live Grep String"
+            }, {
+                "<leader>fs",
+                function()
+                    require("craftzdog.picker").prettyDocumentSymbols()
+                end,
+                desc = "Document Symbols"
+            }, {
+                "<leader>fw",
+                function()
+                    require("craftzdog.picker").prettyWorkspaceSymbols()
+                end,
+                desc = "Document Symbols"
+            }, {
+                "<leader>fb",
+                function()
+                    require("craftzdog.picker").prettyBuffersPicker()
+                end,
+                desc = "Buffer Picker"
+            }, {
+                ";t",
+                function()
+                    local builtin = require("telescope.builtin")
+                    builtin.help_tags()
+                end,
+                desc = "Lists available help tags and opens a new window with the relevant help info on <cr>"
+            }, {
+                ";;",
+                function()
+                    local builtin = require("telescope.builtin")
+                    builtin.resume()
+                end,
+                desc = "Resume the previous telescope picker"
+            }, {
+                ";e",
+                function()
+                    local builtin = require("telescope.builtin")
+                    builtin.diagnostics()
+                end,
+                desc = "Lists Diagnostics for all open buffers or a specific buffer"
+            }, {
+                ";s",
+                function()
+                    local builtin = require("telescope.builtin")
+                    builtin.treesitter()
+                end,
+                desc = "Lists Function names, variables, from Treesitter"
+            }, {
+                "sf",
+                function()
+                    local telescope = require("telescope")
+
+                    local function telescope_buffer_dir()
+                        return vim.fn.expand("%:p:h")
+                    end
+
+                    telescope.extensions.file_browser.file_browser({
+                        path = "%:p:h",
+                        cwd = telescope_buffer_dir(),
+                        respect_gitignore = false,
+                        hidden = true,
+                        grouped = true,
+                        previewer = false,
+                        initial_mode = "normal",
+                        layout_config = {height = 40}
+                    })
+                end,
+                desc = "Open File Browser with the path of the current buffer"
+            }
+        },
+        config = function(_, opts)
+            local telescope = require("telescope")
+            local actions = require("telescope.actions")
+            local fb_actions = require("telescope").extensions.file_browser
+                                   .actions
+
+            opts.defaults = vim.tbl_deep_extend("force", opts.defaults, {
+                wrap_results = true,
+                layout_strategy = "horizontal",
+                layout_config = {prompt_position = "top"},
+                sorting_strategy = "ascending",
+                winblend = 0,
+                mappings = {n = {}}
+            })
+            opts.pickers = {
+                diagnostics = {
+                    theme = "ivy",
+                    initial_mode = "normal",
+                    layout_config = {preview_cutoff = 9999},
+                    sorting = {
+                        previewer = {
+                            get_generic_file = function(entry)
+                                return string.format("%s (%s)",
+                                                     entry.path:match("([^/]+)$"),
+                                                     entry.path)
+                            end
+                        }
+                    }
+                }
+            }
+            opts.extensions = {
+                file_browser = {
+                    theme = "dropdown",
+                    -- disables netrw and use telescope-file-browser in its place
+                    hijack_netrw = true,
+                    mappings = {
+                        -- your custom insert mode mappings
+                        ["n"] = {
+                            -- your custom normal mode mappings
+                            ["N"] = fb_actions.create,
+                            ["h"] = fb_actions.goto_parent_dir,
+                            ["/"] = function()
+                                vim.cmd("startinsert")
+                            end,
+                            ["<C-u>"] = function(prompt_bufnr)
+                                for i = 1, 10 do
+                                    actions.move_selection_previous(prompt_bufnr)
+                                end
+                            end,
+                            ["<C-d>"] = function(prompt_bufnr)
+                                for i = 1, 10 do
+                                    actions.move_selection_next(prompt_bufnr)
+                                end
+                            end,
+                            ["<PageUp>"] = actions.preview_scrolling_up,
+                            ["<PageDown>"] = actions.preview_scrolling_down
+                        }
+                    }
+                }
+            }
+            telescope.setup(opts)
+            require("telescope").load_extension("fzf")
+            require("telescope").load_extension("file_browser")
+        end
+    }
+}
