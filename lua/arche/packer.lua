dda808976485158931288a706d9f8a54d071e1b3
Rahul Biswakarma <62774412+rahul-biswakarma@users.noreply.github.com>
2023-12-29 02:10:43 +0530

    add: lualine

--- lua/arche/packer.lua
+++ lua/arche/packer.lua
@@ -28,4 +28,8 @@ return require('packer').startup(function(use)
             {'hrsh7th/nvim-cmp'}, {'hrsh7th/cmp-nvim-lsp'}, {'L3MON4D3/LuaSnip'}
         }
     }
-end)
+    use({
+        'nvim-lualine/lualine.nvim',
+        requires = {'nvim-tree/nvim-web-devicons', opts = true}
+    })
+end, {display = {open_fn = require('packer.util').float}})
